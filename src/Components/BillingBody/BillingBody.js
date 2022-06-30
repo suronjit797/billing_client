bfe14998fd504a6e94d3a8e0a51f0a956306df7b
suronjit797 <suronjit797@gmail.com>
2022-06-30 14:29:45 +0600

    billing table

--- src/Components/BillingBody/BillingBody.js
+++ src/Components/BillingBody/BillingBody.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import BillingHeader from './BillingHeader';
+import { Table } from 'react-bootstrap'
 
 const BillingBody = () => {
     return (
         <div className='my-3 container'>
             <BillingHeader />
+
+            <Table bordered hover className='billing_table'>
+                <thead>
+                    <tr>
+                        <th>Billing ID</th>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Paid Amount</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Billing ID</td>
+                        <td>Full Name</td>
+                        <td>Email</td>
+                        <td>Phone</td>
+                        <td>Paid Amount</td>
+                        <td>Action</td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     );
 };
--- src/Components/BillingBody/BillingHeader.js
+++ src/Components/BillingBody/BillingHeader.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 const BillingHeader = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg_custom-primary">
-            <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        Paid Total : 0
-                    </ul>
-                </div>
+        <nav className="d-flex px-3 py-1 bg_custom-primary align-items-center">
+            <div className="left">
+                Billing
+            </div>
+            <div className="ms-auto">
+                <button className='px-4 btn_custom-primary'>Add New Bill</button>
             </div>
         </nav>
     );
